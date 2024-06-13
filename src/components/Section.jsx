fd795a8dd66dac19955e69ca1c7a1f910d52e8ea
Kartikey2011yadav <nykay10968@gmail.com>
2024-06-13 20:53:41 +0530

    border styling update

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -26,13 +26,13 @@ const Section = ({
     >
       {children}
 
-      <div className="hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events- md:block lg:left-7.5 xl:left-10" />
-      <div className="hidden absolute top-0 right-5 w-0.25 h-full bg-stroke-1 pointer-events- md:block lg:right-7.5 xl:right-10" />
+      <div className="hidden absolute top-0 left-5 w-0.25 h-full bg-color-5 pointer-events- md:block lg:left-7.5 xl:left-10 -z-20" />
+      <div className="hidden absolute top-0 right-5 w-0.25 h-full bg-color-5 pointer-events- md:block lg:right-7.5 xl:right-10 -z-20" />
 
       {crosses && (
         <>
           <div
-            className={`hidden absolute top-0 left-7.5 right-7.5 h-0.25 bg-stroke-1 ${
+            className={`hidden absolute top-0 left-7.5 right-7.5 h-0.25 bg-color-5 ${
               crossesOffset && crossesOffset
             } pointer-events-none lg:block xl:left-10 xl:right-10`}
           />
