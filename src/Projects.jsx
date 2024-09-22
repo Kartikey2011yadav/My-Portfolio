8d28d037f411eeb3359b00ce47123813cbff457b
Kartikey2011yadav <nykay10968@gmail.com>
2024-09-22 16:34:44 +0530

    restructure

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,23 +1,68 @@
-import {applyVueInReact, applyPureVueInReact} from 'veaury';
-import Features from "./components/Features";
-import Collaboration from "./components/Intro";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Mockup from "./model/Mockup.vue";
-import {useState} from 'react'; 
-// import gamestack-login from "./"
+import styles from './project-summary.module.css';
+import { deviceModels } from './components/model/device-models';
+import { useWindowSize } from './hooks';
+import { Suspense, lazy, useState } from 'react';
+import gamestackTexture2Large from './assets/gamestack-list-large.jpg';
+import gamestackTexture2Placeholder from './assets/gamestack-list-placeholder.jpg';
+import gamestackTexture2 from './assets/gamestack-list.jpg';
+import gamestackTextureLarge from './assets/gamestack-login-large.jpg';
+import gamestackTexturePlaceholder from './assets/gamestack-login-placeholder.jpg';
+import gamestackTexture from './assets/gamestack-login.jpg';
 
-import gamestack from "./assets/gamestack-login.jpg";
+const Model = lazy(() =>
+  import('./components/model').then(module => ({ default: module.Model }))
+);
 
-const Mockup_ = applyVueInReact(Mockup);
+const media = {
+  desktop: 2080,
+  laptop: 1680,
+  tablet: 1040,
+  mobile: 696,
+  mobileS: 400,
+};
+
+// const [modelLoaded, setModelLoaded] = useState(false);
+// const { width } = useWindowSize();
+// const isMobile = width <= media.tablet;
+const phoneSizes = `(max-width: ${media.tablet}px) 30vw, 20vw`;
+const laptopSizes = `(max-width: ${media.tablet}px) 80vw, 40vw`;
+
+// function handleModelLoad() {
+//   setModelLoaded(true);
+// }
 
 const Projects = () => {
   return (
     <>
-      
-        <Mockup_ screen ={gamestack}/>
-
+      <div className={styles.model} data-device="phone">
+        <Model
+          alt={"hello"}
+          cameraPosition={{ x: 0, y: 0, z: 11.5 }}
+          showDelay={300}
+          onLoad={() => { }}
+          show={true}
+          models={[
+            {
+              ...deviceModels.phone,
+              position: { x: -0.6, y: 1.1, z: 0 },
+              texture: {
+                srcSet: `${gamestackTexture} 375w, ${gamestackTextureLarge} 750w`,
+                placeholder: gamestackTexturePlaceholder,
+                sizes: phoneSizes,
+              },
+            },
+            {
+              ...deviceModels.phone,
+              position: { x: 0.6, y: -0.5, z: 0.3 },
+              texture: {
+                srcSet: `${gamestackTexture2} 375w, ${gamestackTexture2Large} 750w`,
+                placeholder: gamestackTexture2Placeholder,
+                sizes: phoneSizes,
+              },
+            },
+          ]}
+        />
+      </div>
     </>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import veauryVitePlugins from 'veaury/vite/index.js';
+import mdx from '@mdx-js/rollup';
+import remarkFrontmatter from 'remark-frontmatter';
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
+import rehypeImgSize from 'rehype-img-size';
+import rehypeSlug from 'rehype-slug';
+import rehypePrism from '@mapbox/rehype-prism';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: "/My-Portfolio",
   plugins: [
     // react(),
+    mdx({
+      rehypePlugins: [[rehypeImgSize, { dir: 'public' }], rehypeSlug, rehypePrism],
+      remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+      providerImportSource: '@mdx-js/react',
+    }),
     veauryVitePlugins({
       type: 'react',
       // Configuration of @vitejs/plugin-vue
