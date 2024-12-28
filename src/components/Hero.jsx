9acc26471aa3abba803d174e6bfffe76c8ee9bf8
Kartikey2011yadav <nykay10968@gmail.com>
2024-12-28 18:48:28 +0530

    rephrase

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -82,7 +82,7 @@ const Hero = () => {
                 <ScrollParallax isAbsolutelyPositioned>
                   <Notification
                     className="hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex"
-                    title="Code generation"
+                    title="Building Apps . . ."
                   />
                 </ScrollParallax>
               </div>
--- src/components/TechLogos.jsx
+++ src/components/TechLogos.jsx
@@ -4,7 +4,7 @@ const TechLogos = ({ className }) => {
   return (
     <div className={className}>
       <h5 className="tagline mb-6 -mt-15 text-center text-n-1/50">
-        Helping people create beautiful content at
+        Building seamless experiences across platforms
       </h5>
       <ul className="flex -mt-1">
         {techLogos.map((logo, index) => (
