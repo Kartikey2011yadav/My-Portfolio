092720d9bf56bbbfca2528b5a9b62d601c8e5fae
Kartikey2011yadav <nykay10968@gmail.com>
2024-12-30 19:11:23 +0530

    removing extra route

--- src/main.jsx
+++ src/main.jsx
@@ -8,18 +8,15 @@ import "./index.css";
 import About from "./About.jsx";
 import Connect from "./Connect.jsx";
 import Test from "./test/Test.jsx";
-import ProjectsList from "./pro.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>  
       <Routes>
-        
         <Route path="/" index element={<Home />}/>
         <Route path="/projects" element={<Projects />}/> // empty for now
         <Route path="/about" element={<About />}/>
         <Route path="/connect" element={<Connect />}/>
-        <Route path="/pro" element={<ProjectsList />}/> // imp
         <Route path="/test" element={<Test />}/>
         
       </Routes>
