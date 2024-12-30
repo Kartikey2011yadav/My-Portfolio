ae1bd09a341923271f3794e7b4527b01772413b8
Kartikey2011yadav <nykay10968@gmail.com>
2024-12-30 19:16:26 +0530

    content not selectable

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router , Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Projects from "./Projects.jsx";
 import Home from "./Home.jsx";
@@ -11,15 +11,14 @@ import Test from "./test/Test.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>  
+    <Router>
       <Routes>
-        <Route path="/" index element={<Home />}/>
-        <Route path="/projects" element={<Projects />}/> // empty for now
-        <Route path="/about" element={<About />}/>
-        <Route path="/connect" element={<Connect />}/>
-        <Route path="/test" element={<Test />}/>
-        
+        <Route path="/" index element={<Home className="non-selectable" />} />
+        <Route path="/projects" element={<Projects className="non-selectable" />} />
+        <Route path="/about" element={<About className="non-selectable" />} />
+        <Route path="/connect" element={<Connect className="non-selectable" />} />
+        <Route path="/test" element={<Test className="non-selectable" />} />
       </Routes>
-    </Router> 
+    </Router>
   </React.StrictMode>
 );
